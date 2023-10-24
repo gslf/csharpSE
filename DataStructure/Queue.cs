c6cead7cee38f18e0cbd4e7be78ff3064aa8ccb1
gioeleslfierro <info@promezio.it>
2023-10-24 21:54:48 +0200

    Added Queue DataStructure

---
+++ DataStructure/Queue.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Collections.Generic;
+
+namespace DataStructure.Queue {
+    /// <summary>
+    /// FIFO Queue, implemented using a List.
+    /// </summary>
+    /// <typeparam name="T">Generic Type.</typeparam>
+    public class Queue<T> {
+        
+        private List<T> queue = new List<T>();
+
+        /// <summary>
+        /// Create a new empty queue
+        /// <example>
+        /// For example:
+        /// <code>
+        /// var my_queue = new DataStructure.Queue.Queue<int>();
+        /// </code>
+        /// </example>
+        /// </summary>
+        public Queue() {
+            queue = new List<T>();
+        }
+
+        /// <summary>
+        /// Create a new queue from an Enumerable
+        /// <example>
+        /// For example:
+        /// <code>
+        /// var enumerable_queue = new DataStructure.Queue.Queue<int>(new[] { 0, 1, 2, 3, 4 });
+        /// </code>
+        /// </example>
+        /// </summary>
+        public Queue(IEnumerable<T> items) {
+            queue = new List<T>();
+            foreach (var item in items) {
+                queue.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Get the size of the queue
+        /// <returns>The size of the queue</returns>
+        /// </summary>
+        public int Size() {
+            return queue.Count;
+        }
+
+        /// <summary>
+        /// Add a new item to the queue
+        /// <param name="item">The item to enqueue</param>
+        /// </summary>
+        public void Enqueue(T item) {
+            queue.Add(item);
+        }
+
+        /// <summary>
+        /// Get (and remove) an item from the queue
+        /// <returns>The removed item</returns>
+        /// </summary>>
+        public T Dequeue() {
+            if (queue.Count < 1) throw new InvalidOperationException();
+
+            var first = queue[0];
+            queue.RemoveAt(0);
+            return first;
+        }
+
+        /// <summary>
+        /// Get an item from the queue
+        /// <returns>The peeked item</returns>
+        /// </summary>
+        public T Peek() {
+            if (queue.Count < 1) throw new InvalidOperationException();
+
+            return queue[0];
+        }
+
+        /// <summary>
+        /// Remove all items from the queue
+        /// </summary>
+        public void Clear() {
+            queue.Clear();
+        }
+    }
+}
---
+++ DataStructureTests/QueueTest.cs
@@ -0,0 +1,30 @@
+﻿using DataStructure.Queue;
+
+namespace DataStructureTests {
+    [TestClass]
+    public class QueueTest {
+        [TestMethod]
+        public void TestConstructor() {
+            var empty_queue = new DataStructure.Queue.Queue<int>();
+            var enumerable_queue = new DataStructure.Queue.Queue<int>(new[] { 0, 1, 2, 3, 4 });
+            Assert.AreEqual(0, empty_queue.Size());
+            Assert.AreEqual(0, enumerable_queue.Peek());
+        }
+
+        [TestMethod]
+        public void TestPushPop() {
+            var my_queue = new DataStructure.Queue.Queue<int>();
+            my_queue.Enqueue(1);
+            my_queue.Enqueue(2);
+            Assert.AreEqual(1, my_queue.Dequeue());
+
+            try {
+                my_queue.Clear();
+                my_queue.Dequeue();
+                Assert.Fail("no exception thrown");
+            } catch (Exception ex) {
+                Assert.IsTrue(ex is InvalidOperationException, ex.Message);
+            }
+        }
+    }
+}
