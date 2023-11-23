f080c2f22ebe2e342a8f17fa759d6b5e799846fd
gioeleslfierro <info@promezio.it>
2023-11-23 19:11:19 +0100

    Adding Knapsack problem in Algorithms

---
+++ Algorithm/Knapsack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Knapsack {
+    public class Knapsack {
+        /// <summary>
+        /// The Knapsack Problem is a pivotal optimization challenge in combinatorial 
+        /// optimization and dynamic programming. It entails optimizing the total 
+        /// value of items packed in a backpack, constrained by its weight capacity. 
+        /// In its most encountered form, the "0/1 Knapsack Problem," each item can 
+        /// either be entirely included or excluded, with no partial selection. 
+        /// </summary>
+        /// <param name="weight">Items weight</param>
+        /// <param name="value">Items values</param>
+        /// <param name="capacity">The backpack capacity</param>
+        /// <returns></returns>
+        public int MaxValue(int[] weight, int[] value, int capacity) {
+            
+            // The number of items
+            int n = weight.Length;
+
+            // Creates a matrix of size (n + 1) x (capacity + 1) to store the intermediate
+            //results. The matrix is used for dynamic programming.
+            int[,] dp = new int[n + 1, capacity + 1];
+
+            // Iterate over all items
+            for (int i = 0; i <= n; i++) {
+
+                // Iterate over all backback capacity results
+                for (int w = 0; w <= capacity; w++) {
+
+                    // Initialize the first row and first column of the matrix to 0.
+                    // This represents the base case: no items or no backpack capacity.
+                    if (i == 0 || w == 0)
+                        dp[i, w] = 0;
+
+                    // Check if the current item's weight can be accommodated in the
+                    // backpack. If yes, choose the maximum between including the current
+                    // item or not including it.
+                    else if (weight[i - 1] <= w)
+                        dp[i, w] = Math.Max(value[i - 1] + dp[i - 1, w - weight[i - 1]], dp[i - 1, w]);
+
+                    //If the current item's weight is too large to fit in the backpack,
+                    //use the value of the optimal solution without the current item.
+                    else
+                        dp[i, w] = dp[i - 1, w];
+                }
+            }
+
+            return dp[n, capacity];
+        }
+    }
+}
---
+++ AlgorithmTests/KnapsackTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm.Knapsack;
+
+namespace AlgorithmTests {
+    [TestClass]
+    public class KnapsackTest {
+        private readonly Knapsack _knapsack = new Knapsack();
+
+        [TestMethod]
+        public void TestEmptyKnapsack() {
+            int[] values = { };
+            int[] weights = { };
+            int capacity = 0;
+            Assert.AreEqual(0, _knapsack.MaxValue(weights, values, capacity));
+        }
+
+        [TestMethod]
+        public void TestSingleItemFits() {
+            int[] values = { 100 };
+            int[] weights = { 10 };
+            int capacity = 50;
+            Assert.AreEqual(100, _knapsack.MaxValue(weights, values, capacity));
+        }
+
+        [TestMethod]
+        public void TestSingleItemDoesNotFit() {
+            int[] values = { 100 };
+            int[] weights = { 60 };
+            int capacity = 50;
+            Assert.AreEqual(0, _knapsack.MaxValue(weights, values, capacity));
+        }
+
+        [TestMethod]
+        public void TestMultipleItems() {
+            int[] values = { 60, 100, 120 };
+            int[] weights = { 10, 20, 30 };
+            int capacity = 50;
+            Assert.AreEqual(220, _knapsack.MaxValue(weights, values, capacity));
+        }
+
+        [TestMethod]
+        public void TestFullCapacityNotUsed() {
+            int[] values = { 60, 40 };
+            int[] weights = { 10, 20 };
+            int capacity = 50;
+            Assert.AreEqual(100, _knapsack.MaxValue(weights, values, capacity));
+        }
+
+        [TestMethod]
+        public void TestZeroCapacity() {
+            int[] values = { 60, 100, 120 };
+            int[] weights = { 10, 20, 30 };
+            int capacity = 0;
+            Assert.AreEqual(0, _knapsack.MaxValue(weights, values, capacity));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestInvalidInput() {
+            int[] values = { 60, 100 }; // Length mismatch
+            int[] weights = { 10, 20, 30 };
+            int capacity = 50;
+            _knapsack.MaxValue(weights, values, capacity);
+        }
+    }
+}
